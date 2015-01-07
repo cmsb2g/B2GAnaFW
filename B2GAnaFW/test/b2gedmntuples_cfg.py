49ad96ca9c79312499ced6e7bb300cf4aa35908a
decosa <decosa@cern.ch>
2015-01-07 11:10:11 +0100

    add btag to ak8 - missing substructure info

--- B2GAnaFW/test/b2gedmntuples_cfg.py
+++ B2GAnaFW/test/b2gedmntuples_cfg.py
@@ -56,6 +56,7 @@ muLabel  = 'slimmedMuons'
 elLabel  = 'slimmedElectrons'
 jLabel = 'slimmedJets'
 jLabelAK8 = 'slimmedJetsAK8'
+ak8jetLabel = 'patJetsSlimmedJetsAK8BTagged'
 pvLabel  = 'offlineSlimmedPrimaryVertices'
 particleFlowLabel = 'packedPFCandidates'    
 metLabel = 'slimmedMETs'
@@ -83,6 +84,10 @@ process.source = cms.Source("PoolSource",
         )
 )
 
+process.load("PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff")
+process.load("Configuration.EventContent.EventContent_cff")
+process.load('Configuration.StandardSequences.Geometry_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 from Configuration.AlCa.GlobalTag import GlobalTag as customiseGlobalTag
 process.GlobalTag = customiseGlobalTag(process.GlobalTag, globaltag = 'auto:startup_GRun')
@@ -90,10 +95,84 @@ process.GlobalTag.connect   = 'frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'
 process.GlobalTag.pfnPrefix = cms.untracked.string('frontier://FrontierProd/')
 for pset in process.GlobalTag.toGet.value():
     pset.connect = pset.connect.value().replace('frontier://FrontierProd/', 'frontier://FrontierProd/')
-    #   Fix for multi-run processing:
-    process.GlobalTag.RefreshEachRun = cms.untracked.bool( False )
-    process.GlobalTag.ReconnectEachRun = cms.untracked.bool( False )
+#   Fix for multi-run processing:
+process.GlobalTag.RefreshEachRun = cms.untracked.bool( False )
+process.GlobalTag.ReconnectEachRun = cms.untracked.bool( False )
     
+
+
+#for Inclusive Vertex Finder
+process.load("RecoBTag/Configuration/RecoBTag_cff")
+process.load('RecoVertex/AdaptiveVertexFinder/inclusiveVertexing_cff')
+process.inclusiveVertexFinder.tracks = cms.InputTag("unpackedTracksAndVertices")
+process.inclusiveVertexFinder.primaryVertices = cms.InputTag("unpackedTracksAndVertices")
+process.trackVertexArbitrator.tracks = cms.InputTag("unpackedTracksAndVertices")
+process.trackVertexArbitrator.primaryVertices = cms.InputTag("unpackedTracksAndVertices")
+
+#new input for impactParameterTagInfos, softleptons, IVF
+process.impactParameterTagInfos.jetTracks = cms.InputTag("jetTracksAssociatorAtVertexSlimmedJetsAK8BTagged")
+process.impactParameterTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
+process.inclusiveVertexFinder.primaryVertices = cms.InputTag("unpackedTracksAndVertices")
+process.trackVertexArbitrator.primaryVertices = cms.InputTag("unpackedTracksAndVertices")
+process.softPFMuonsTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
+process.softPFElectronsTagInfos.primaryVertex = cms.InputTag("unpackedTracksAndVertices")
+process.softPFMuonsTagInfos.jets = cms.InputTag("patJetsSlimmedJetsAK8BTagged")
+process.softPFElectronsTagInfos.jets = cms.InputTag("patJetsSlimmedJetsAK8BTagged") 
+process.inclusiveSecondaryVertexFinderTagInfosV2 = process.inclusiveSecondaryVertexFinderTagInfos.clone()
+process.inclusiveSecondaryVertexFinderTagInfosV2.trackSelection.qualityClass = cms.string('any')
+
+from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
+addJetCollection(
+    process,
+    postfix   = "",
+    labelName = 'SlimmedJetsAK8BTagged',
+    jetSource = cms.InputTag('slimmedJetsAK8'),
+    trackSource = cms.InputTag('unpackedTracksAndVertices'),
+    pfCandidates = cms.InputTag('packedPFCandidates'), 
+    pvSource = cms.InputTag('unpackedTracksAndVertices'), 
+    jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'Type-2'),
+    btagDiscriminators = [      'combinedSecondaryVertexBJetTags', 'combinedInclusiveSecondaryVertexV2BJetTags'     ]
+    ,algo= 'AK', rParam = 0.8
+    )
+
+process.patJetFlavourAssociation.jets = "slimmedJetsAK8"
+process.patJetFlavourAssociation.rParam = 0.8 
+
+#adjust MC matching
+process.patJetGenJetMatchSlimmedJetsAK8BTagged.matched = "slimmedGenJets"
+process.patJetPartonMatchSlimmedJetsAK8BTagged.matched = "prunedGenParticles"
+process.patJetPartons.particles = "prunedGenParticles"
+
+#adjust PV used for Jet Corrections
+process.patJetCorrFactorsSlimmedJetsAK8BTagged.primaryVertices = "unpackedTracksAndVertices"
+
+#adjust JTA cone size 
+process.jetTracksAssociatorAtVertexSlimmedJetsAK8BTagged.coneSize = 0.8 
+
+process.load('PhysicsTools.PatAlgos.slimming.unpackedTracksAndVertices_cfi')
+process.combinedSecondaryVertex.trackMultiplicityMin = 1 #silly sv, uses un filtered tracks.. i.e. any pt
+
+##
+## Jet substructure variables as user floats
+##
+#process.patJetsSlimmedJetsAK8BTagged.userData.userFloats.src = []
+#process.selectedPatJetsSlimmedJetsAK8BTagged.cut = cms.string("pt > 100")
+#from RecoJets.Configuration.RecoPFJets_cff import ak8PFJetsCHSPruned, ak8PFJetsCHSFiltered, ak8PFJetsCHSTrimmed
+#process.ak8PFJetsCHSPruned   = ak8PFJetsCHSPruned.clone()
+#process.ak8PFJetsCHSTrimmed  = ak8PFJetsCHSTrimmed.clone()
+#process.ak8PFJetsCHSFiltered = ak8PFJetsCHSFiltered.clone()
+#
+#process.load("RecoJets.JetProducers.ak8PFJetsCHS_groomingValueMaps_cfi")
+#process.ak8PFJetsCHSPrunedLinks.src  = cms.InputTag("slimmedJetsAK8")
+#process.ak8PFJetsCHSTrimmedLinks.src  = cms.InputTag("slimmedJetsAK8")
+#process.ak8PFJetsCHSFilteredLinks.src = cms.InputTag("slimmedJetsAK8")
+#
+#process.patJetsSlimmedJetsAK8BTagged.userData.userFloats.src += ['ak8PFJetsCHSPrunedLinks','ak8PFJetsCHSTrimmedLinks','ak8PFJetsCHSFilteredLinks']
+#
+#process.load('RecoJets.JetProducers.nJettinessAdder_cfi')
+#process.NjettinessAK8 = process.Njettiness.clone(src=cms.InputTag("slimmedJetsAK8"),)
+#process.NjettinessAK8.cone = cms.double(0.8)
+#process.patJetsSlimmedJetsAK8BTagged.userData.userFloats.src += ['NjettinessAK8:tau1','NjettinessAK8:tau2','NjettinessAK8:tau3']
 
 ### Selected leptons and jets
 process.skimmedPatMuons = cms.EDFilter(
@@ -123,10 +202,19 @@ process.skimmedPatJets = cms.EDFilter(
 
 
 process.skimmedPatJetsAK8 = cms.EDFilter(
-    "PATJetSelector",
-    src = cms.InputTag(jLabelAK8),
-    cut = cms.string("pt > 100 && abs(eta) < 4.")
-)
+    "CandViewSelector",
+    src = cms.InputTag(ak8jetLabel),
+    #    src = cms.InputTag("goodPatJets"), # 71x    
+    cut = cms.string("pt > 25 && abs(eta) < 4.")
+    )
+
+
+#process.skimmedPatJetsAK8 = cms.EDFilter(
+#    "PATJetSelector",
+#    src = cms.InputTag(jLabelAK8),
+#    cut = cms.string("pt > 100 && abs(eta) < 4.")
+#)
+
 
 
 ### Asking for at least 2 jets satisfying the selection above 
@@ -200,6 +288,9 @@ process.centrality = cms.EDProducer("CentralityUserData",
 ### Including ntuplizer 
 process.load("B2GAnaFW.B2GAnaFW.b2gedmntuples_cff")
 
+process.options.allowUnscheduled = cms.untracked.bool(True)
+
+
 ### definition of Analysis sequence
 process.analysisPath = cms.Path(
     process.skimmedPatElectrons +
@@ -217,7 +308,7 @@ process.analysisPath = cms.Path(
 
 process.analysisPath+=process.muonUserData
 process.analysisPath+=process.jetUserData
-process.analysisPath+=process.jetUserDataAK8
+#process.analysisPath+=process.jetUserDataAK8
 process.analysisPath+=process.electronUserData
 process.analysisPath+=process.genPart
 process.analysisPath+=process.muons
