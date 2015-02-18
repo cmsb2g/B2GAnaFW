6e80ed40d537ff733d9868abf249ee835fb3d832
Salvatore Rappoccio <rappoccio@gmail.com>
2015-02-18 14:23:09 -0600

    Adding some crab configs

---
+++ test/CRAB/crab_zprime3TeV_w30GeV.py
@@ -0,0 +1,26 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'zprime3TeV_w30GeV'
+config.General.workArea = 'PHYS14'
+config.General.transferLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'b2gedmntuples_cfg.py'
+config.JobType.pyCfgParams = ['LHE=0']
+
+config.section_("Data")
+config.Data.inputDataset = '/ZPrimeToTTJets_M3000GeV_W30GeV_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.ignoreLocality = True
+config.Data.publication = True
+# This string is used to construct the output dataset name
+config.Data.publishDBS = 'phys03'
+config.Data.publishDataName = 'B2GAnaFW_PHYS14'
+
+config.section_("Site")
+config.Site.storageSite = 'T3_US_FNALLPC'
+
