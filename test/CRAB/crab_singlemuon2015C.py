2cf0ea9b21d9cd51e6e01533a1c3811e60d2e81f
Devdatta <devdatta.majumder@cern.ch>
2015-09-25 18:30:05 +0200

    CRAB3 config file to run on SingleMuon 2015D dataset

--- test/CRAB/crab_singlemuon2015C.py
+++ test/CRAB/crab_singlemuon2015C.py
@@ -2,7 +2,7 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'jetht2015C'
+config.General.requestName = 'singlemuon2015C'
 config.General.workArea = 'B2GAnaFW_v74x_V7_25ns'
 config.General.transferLogs = True
 
@@ -13,7 +13,7 @@ config.JobType.pyCfgParams = ['maxEvents=-1', 'DataProcessing=Data25nsv2']
 config.JobType.inputFiles = ['Cert_246908-251252_13TeV_PromptReco_Collisions15_JSON.txt']
 
 config.section_("Data")
-config.Data.inputDataset = '/JetHT/Run2015D-PromptReco-v3/MINIAOD' 
+config.Data.inputDataset = '/SingleMuon/Run2015D-PromptReco-v3/MINIAOD' 
 config.Data.splitting = 'LumiBased'
 config.Data.inputDBS = 'global'
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-256869_13TeV_PromptReco_Collisions15_25ns_JSON.txt' 
@@ -23,7 +23,7 @@ config.Data.publication = True
 # This string is used to construct the output dataset name
 config.Data.publishDBS = 'phys03'
 config.Data.publishDataName = 'B2GAnaFW_v74x_V7_25ns'
-config.Data.outLFNDirBase = '/store/user/devdatta/B2GAnaFW/JetHT/Run2015D-PromptReco-v3' 
+config.Data.outLFNDirBase = '/store/user/devdatta/B2GAnaFW/SingleMuon/Run2015D-PromptReco-v3' 
 
 config.section_("Site")
 config.Site.storageSite = 'T3_US_FNALLPC'
