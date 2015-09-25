6e6ca5ac78d7be254aceb067e3ba623449c2d04b
Devdatta <devdatta.majumder@cern.ch>
2015-09-25 18:10:15 +0200

    CRAB3 config file to run on 2015D dataset

---
+++ test/CRAB/crab_jetht2015C.py
@@ -0,0 +1,30 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'jetht2015C'
+config.General.workArea = 'B2GAnaFW_v74x_V7'
+config.General.transferLogs = True
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'b2gedmntuples_cfg.py'
+config.JobType.pyCfgParams = ['maxEvents=-1', 'DataProcessing=Data25nsv2']
+config.JobType.inputFiles = ['Cert_246908-251252_13TeV_PromptReco_Collisions15_JSON.txt']
+
+config.section_("Data")
+config.Data.inputDataset = '/JetHT/Run2015D-PromptReco-v3/MINIAOD' 
+config.Data.splitting = 'LumiBased'
+config.Data.inputDBS = 'global'
+config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-256869_13TeV_PromptReco_Collisions15_25ns_JSON.txt' 
+config.Data.unitsPerJob = 20
+config.Data.ignoreLocality = False
+config.Data.publication = True
+# This string is used to construct the output dataset name
+config.Data.publishDBS = 'phys03'
+config.Data.publishDataName = 'B2GAnaFW_v74x_V7'
+config.Data.outLFNDirBase = '/store/user/devdatta/B2GAnaFW/JetHT/Run2015D-PromptReco-v3' 
+
+config.section_("Site")
+config.Site.storageSite = 'T3_US_FNALLPC'
+
