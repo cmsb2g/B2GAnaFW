6379c46aed05f1015a1076d723ca84cd56676a7a
decosa <decosa@cern.ch>
2015-09-29 19:39:27 +0200

    adding an example of crab file for MC

--- test/CRAB/crab_TT.py
+++ test/CRAB/crab_TT.py
@@ -8,7 +8,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '../b2gedmntuples_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
 config.JobType.pyCfgParams = ['DataProcessing=MC25ns','lheLabel=externalLHEProducer']
-config.JobType.inputFiles = ['Summer15_25nsV2_DATA.db','Summer15_25nsV2_MC.db','Summer15_50nsV2_MC.db','Summer15_50nsV4_DATA.db','Summer15_50nsV4_MC.db','Summer15_50nsV5_DATA.db','Summer15_50nsV5_MC.db']
+config.JobType.inputFiles = ['../Summer15_25nsV2_DATA.db','../Summer15_25nsV2_MC.db','../Summer15_50nsV2_MC.db','../Summer15_50nsV4_DATA.db','../Summer15_50nsV4_MC.db','../Summer15_50nsV5_DATA.db','../Summer15_50nsV5_MC.db']
 
 config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
 
